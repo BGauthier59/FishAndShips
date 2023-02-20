26f2405318c8d9b6e23c4270b6cf5fc945e7dad8
BGauthier59 <g.baudinet@rubika-edu.com>
2023-02-20 12:25:09 +0100

    Swipe scenes proto

--- Mobile_G3/Assets/Scripts/GAB/Ex.cs
+++ Mobile_G3/Assets/Scripts/GAB/Ex.cs
@@ -30,4 +30,29 @@ public static class Ex
     }
 
     #endregion
+
+    #region Euler Angles
+
+    public static void SetEulerAnglesX(this Transform tr, float x)
+    {
+        var pos = tr.eulerAngles;
+        pos.x = x;
+        tr.eulerAngles = pos;
+    }
+
+    public static void SetEulerAnglesY(this Transform tr, float y)
+    {
+        var pos = tr.eulerAngles;
+        pos.y = y;
+        tr.eulerAngles = pos;
+    }
+
+    public static void SetEulerAnglesZ(this Transform tr, float z)
+    {
+        var pos = tr.eulerAngles;
+        pos.z = z;
+        tr.eulerAngles = pos;
+    }
+
+    #endregion
 }
--- Mobile_G3/Assets/Scripts/GAB/Swipe/CircularSwipeManager.cs
+++ Mobile_G3/Assets/Scripts/GAB/Swipe/CircularSwipeManager.cs
@@ -9,33 +9,59 @@ using UnityEngine.InputSystem;
 public class CircularSwipeManager : MonoBehaviour
 {
     [SerializeField] private Transform centralPoint;
+    [SerializeField] private SpriteRenderer rotatingPointRd;
+
+    [SerializeField] private float2 minMaxMagnitude;
+
+
+    [SerializeField] private Transform availableZone;
+    [SerializeField] private Transform unavailableCenterZone;
+    private float2 minMaxSqrMagnitude;
 
     private Vector2 startTouch, currentTouch;
-    private Vector2 startVector, currentVector, lastVector;
+    private Vector2 startVector, currentVector;
     private bool isDraging;
 
     private float currentAngle;
     private int circleCount;
+
+    private void OnValidate()
+    {
+        minMaxSqrMagnitude.x = math.pow(minMaxMagnitude.x, 2);
+        minMaxSqrMagnitude.y = math.pow(minMaxMagnitude.y, 2);
+
+        availableZone.localScale = new Vector3(minMaxMagnitude.y * 2, minMaxMagnitude.y * 2, 1);
+        unavailableCenterZone.localScale = new Vector3(minMaxMagnitude.x * 2, minMaxMagnitude.x * 2, 1);
+    }
+
+    private void Start()
+    {
+        OnValidate();
+    }
 
     [UsedImplicitly]
     public void OnTapOnScreen(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
         {
-            isDraging = true;
             startTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             startVector = startTouch - (Vector2) centralPoint.position;
-            lastVector = startVector;
 
-            // Todo - Should verify if magnitude if big enough
+            Debug.Log(startVector.sqrMagnitude);
+            if (startVector.sqrMagnitude < minMaxSqrMagnitude.x ||
+                startVector.sqrMagnitude > minMaxSqrMagnitude.y) return;
 
+            isDraging = true;
             startVector.Normalize();
+            rotatingPointRd.transform.localPosition = startTouch;
+            rotatingPointRd.enabled = true;
         }
         else if (ctx.canceled)
         {
             isDraging = false;
-            startTouch = currentTouch = startVector = currentVector = lastVector = Vector2.zero;
+            startTouch = currentTouch = startVector = currentVector = Vector2.zero;
             currentAngle = 0;
+            rotatingPointRd.enabled = false;
         }
     }
 
@@ -47,53 +73,39 @@ public class CircularSwipeManager : MonoBehaviour
             currentTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             currentVector = currentTouch - (Vector2) centralPoint.position;
 
-            // Todo - Should verify if magnitude if big enough
+            if (currentVector.sqrMagnitude < minMaxSqrMagnitude.x ||
+                currentVector.sqrMagnitude > minMaxSqrMagnitude.y) return;
 
             currentVector.Normalize();
 
-            var angleGap = Vector2.Angle(lastVector, currentVector);
-            
-            var last2float3 = new float3(lastVector.x, lastVector.y, 0);
+            var angleGap = Vector2.Angle(startVector, currentVector);
+
+            var last2float3 = new float3(startVector.x, startVector.y, 0);
             var current2float3 = new float3(currentVector.x, currentVector.y, 0);
-            
             var cross = math.cross(last2float3, current2float3);
 
-            if (cross.z > 0)
-            {
-                currentAngle += angleGap;
-            }
-            else
-            {
-                currentAngle -= angleGap;
-            }
-
+            if (cross.z > 0) currentAngle += angleGap;
+            else currentAngle -= angleGap;
+            
             if (currentAngle > 360)
             {
                 circleCount++;
                 currentAngle -= 360;
             }
-            else if(currentAngle < 0 && circleCount != 0)
+            else if (currentAngle < 0 && circleCount != 0)
             {
                 circleCount--;
                 currentAngle += 360;
             }
-            
-            lastVector = currentVector;
+            else if (currentAngle < 0 && circleCount == 0)
+            {
+                currentAngle += 360;
+            }
+
+            startVector = currentVector;
+            centralPoint.SetEulerAnglesZ(currentAngle);
         }
 
         Debug.DrawRay(centralPoint.position, currentVector, Color.yellow);
     }
-
-    /*    
-    Comment coder le swipe circulaire ?
-
-    -Définir un point central du cercle
-
-    -Quand on touche, récupérer les position de l’écran
-
-    -Vecteur Point Central / Position du toucher, Vecteur Point Central / Position current (mouse Position)
-
-    -Calculer l’angle entre ces deux vecteurs normalisés, et ajouter l’écart à une variable pour incrémenter.
-        
-    */
 }
