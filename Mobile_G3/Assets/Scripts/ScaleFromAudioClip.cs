e5770a3bcc3469bbe37eceb982a301f26feac3ba
BGauthier59 <g.baudinet@rubika-edu.com>
2023-02-22 10:07:12 +0100

    Microphone WIP

--- Mobile_G3/Assets/Scripts/ScaleFromAudioClip.cs
+++ Mobile_G3/Assets/Scripts/ScaleFromAudioClip.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ScaleFromAudioClip : MonoBehaviour
@@ -9,6 +11,10 @@ public class ScaleFromAudioClip : MonoBehaviour
     [SerializeField] private float loudnessSensibility = 100;
     [SerializeField] private float threshold = .1f;
 
+    [SerializeField] private TMP_Text loudnessText;
+    [SerializeField] private Gradient loudnessGradient;
+    [SerializeField] private SpriteRenderer rd;
+    
     private float loudness;
 
     void Update()
@@ -19,7 +25,8 @@ public class ScaleFromAudioClip : MonoBehaviour
         {
             loudness = 0;
         }
-        
-        Debug.Log(loudness);
+
+        loudnessText.text = loudness.ToString("F2");
+        rd.color = loudnessGradient.Evaluate(loudness / loudnessSensibility);
     }
 }
