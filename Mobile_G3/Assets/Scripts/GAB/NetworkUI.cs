383b6eab304f1bb780e3d5a0ea5478f983442f2b
Mathis Jibert <mathis.jibert@gmail.com>
2023-02-24 17:41:47 +0100

    Menus Part 1

--- Mobile_G3/Assets/Scripts/GAB/NetworkUI.cs
+++ Mobile_G3/Assets/Scripts/GAB/NetworkUI.cs
@@ -15,7 +15,7 @@ public class NetworkUI : MonoBehaviour
     [SerializeField] private UnityTransport transport;
     [SerializeField] private TMP_Text ipText;
     private string ipToConnect;
-    
+
     #region Buttons
 
     public void StartHost()
@@ -40,27 +40,30 @@ public class NetworkUI : MonoBehaviour
             return;
         }
 
-        // TODO - Check if room full
-        
+
         transport.SetConnectionData(ipToConnect, transport.ConnectionData.Port);
         NetworkManager.Singleton.StartClient();
+        ipText.text = $"IP: {ipToConnect}";
+        
+        // TODO - Check if room full (chercher callback)
     }
 
     public void OnSetIP(string ip)
     {
+        Debug.Log(ip + " Set");
         ipToConnect = ip;
     }
-    
+
     #endregion
-    
+
     string GetIPv4AddressMobile()
     {
         Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
         socket.Connect("8.8.8.8", 65530);
-        
+
         IPEndPoint endpoint = socket.LocalEndPoint as IPEndPoint;
         IPAddress ipAddress = endpoint.Address;
-        
+
         var ip = ipAddress.ToString();
         socket.Close();
 
