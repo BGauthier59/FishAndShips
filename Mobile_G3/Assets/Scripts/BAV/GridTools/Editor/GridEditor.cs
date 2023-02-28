52ef6508cacd7c7c61c60af3dc0e751423f93333
BaptisteV <86929180+MadBrosos@users.noreply.github.com>
2023-02-28 19:29:58 +0100

    Push Selected Button

--- Mobile_G3/Assets/Scripts/BAV/GridTools/Editor/GridEditor.cs
+++ Mobile_G3/Assets/Scripts/BAV/GridTools/Editor/GridEditor.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Serialization;
 
 public class GridEditor : EditorWindow
 {
@@ -9,14 +11,18 @@ public class GridEditor : EditorWindow
     public int rows = 10; 
     public int cols = 10; 
     public bool useCenterOffset = false; 
-    public Vector3 offset = new Vector3(2f, 0f, 2f); 
+    public Vector3 offset = new Vector3(0f, 0f, 0f);
     public Vector3 centerPosition = Vector3.zero;
     public bool showPreview = false;
+    public Vector3 offsetPreviewPosition = new Vector3(0f, 1f, 0f);
+    public bool showSpecificObjetPreview = false;
     
     //Private Variable
     private GridManager _gridManager;
+    private GameObject _selectedObject;
+    private List<Tile> _listGridManagerTile;
 
-    [MenuItem("Window/Grid Editor")]
+    [MenuItem("Tools/Level Design/Grid Editor")]
     public static void ShowWindow()
     {
         GridEditor window = (GridEditor)EditorWindow.GetWindow(typeof(GridEditor));
@@ -44,17 +50,28 @@ public class GridEditor : EditorWindow
         }
         
         showPreview = EditorGUILayout.Toggle("Show Preview", showPreview);
+        offsetPreviewPosition = EditorGUILayout.Vector3Field("Offset Preview Position", offsetPreviewPosition);
         if (showPreview)
         {
-            SceneView.onSceneGUIDelegate += OnSceneGUI;
+            SceneView.duringSceneGui += ShowAllHandlesButton;
         }
         else
         {
-            SceneView.onSceneGUIDelegate -= OnSceneGUI;
+            SceneView.duringSceneGui -= ShowAllHandlesButton;
+        }
+
+        showSpecificObjetPreview = EditorGUILayout.Toggle("Show Preview Selected", showSpecificObjetPreview);
+        if (showSpecificObjetPreview)
+        {
+            SceneView.duringSceneGui += ShowHandlesButton;
+        }
+        else
+        {
+            SceneView.duringSceneGui -= ShowHandlesButton;
         }
     }
     
-    private void OnSceneGUI(SceneView sceneView)
+    private void ShowAllHandlesButton(SceneView sceneView)
     {
         if (!showPreview)
         {
@@ -68,14 +85,40 @@ public class GridEditor : EditorWindow
         {
             for (int col = 0; col < cols; col++)
             {
-                Vector3 position = new Vector3(col, 0, row) * cellSize - center + offset;
-
-                // Dessiner un bouton Handles au-dessus de la cellule
-                if (Handles.Button(position, Quaternion.identity, cellSize * 0.5f, cellSize, Handles.RectangleHandleCap))
+                Vector3 position = new Vector3(col, 0, row) * cellSize - center + offsetPreviewPosition;
+                if (row > 0 && row < rows - 1 && col > 0 && row < rows - 1)
                 {
-                    Debug.Log("Button clicked at position " + position);
+                    // Dessiner un bouton Handles au-dessus de la cellule
+                    if (Handles.Button(position, Quaternion.identity, cellSize * 0.5f, cellSize,
+                            Handles.RectangleHandleCap))
+                    {
+                        Debug.Log("Button clicked at position " + position);
+                    }
                 }
             }
+        }
+    }
+
+    public void ShowHandlesButton(SceneView sceneView)
+    {
+        if (!Selection.activeGameObject) return;
+        UpdateListTileManage();
+        _selectedObject = Selection.activeGameObject;
+        bool isSelectedObjectInList = false;
+        foreach (Tile obj in _listGridManagerTile)
+        {
+
+            if (obj.name == _selectedObject.name)
+            {
+                isSelectedObjectInList = true;
+                break;
+            }
+        }
+        if (!isSelectedObjectInList) return;
+        Vector3 handlePosition = new Vector3(_selectedObject.transform.position.x, 1f, _selectedObject.transform.position.z);
+        if (Handles.Button(handlePosition, Quaternion.identity, cellSize * 0.5f, cellSize, Handles.RectangleHandleCap))
+        {
+            Debug.Log("Meeeh");
         }
     }
 
@@ -88,11 +131,14 @@ public class GridEditor : EditorWindow
         _gridManager.tiles = new List<Tile>(0);
         _gridManager.xSize = rows;
         _gridManager.ySize = cols;
-        
-        
+        PositionGridObject(gridObject);
+    }
+
+    private void PositionGridObject(GameObject gridObject)
+    {
         Vector3 centerOffset = new Vector3(cols / 2f - 0.5f, 0, rows / 2f - 0.5f) * cellSize;
         Vector3 center = useCenterOffset ? centerPosition + centerOffset : centerPosition;
-        
+
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < cols; col++)
@@ -105,20 +151,22 @@ public class GridEditor : EditorWindow
                     Vector3 position = new Vector3(col, 0, row) * cellSize - center + offset;
 
                     tile.obj = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                    tile.obj.name = "Tile " + row + "," + col;
                     tile.obj.transform.position = position;
 
                     tile.obj.transform.parent = gridObject.transform;
                 }
                 else
                 {
-                    tile.type = TileType.Wall; 
+                    tile.type = TileType.Wall;
                 }
+                
                 _gridManager.tiles.Add(tile);
             }
         }
     }
-    
-    
+
+
     private void UpdateGrid()
     {
         GameObject gridObject = GameObject.Find("Grid");
@@ -132,21 +180,20 @@ public class GridEditor : EditorWindow
         {
             Transform child = gridObject.transform.GetChild(0);
             DestroyImmediate(child.gameObject);
+            _gridManager.tiles.Clear();
         }
-        
-        Vector3 centerOffset = new Vector3(cols / 2f - 0.5f, 0, rows / 2f - 0.5f) * cellSize;
-        Vector3 center = useCenterOffset ? centerPosition + centerOffset : centerPosition;
-        
-        for (int row = 0; row < rows; row++)
+        _gridManager.xSize = rows;
+        _gridManager.ySize = cols;
+        PositionGridObject(gridObject);
+    }
+
+    private void UpdateListTileManage()
+    {
+        if (_listGridManagerTile.Count < 1)
         {
-            for (int col = 0; col < cols; col++)
+            for (int i = 0; i < _gridManager.tiles.Count; i++)
             {
-                Vector3 position = new Vector3(col, 0, row) * cellSize - center + offset;
-                
-                GameObject cell = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-                cell.transform.position = position;
-                
-                cell.transform.parent = gridObject.transform;
+                _listGridManagerTile.Add(_gridManager.tiles[i]);
             }
         }
     }
