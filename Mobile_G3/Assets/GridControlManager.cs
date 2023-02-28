d885dfd7b10c4e0ac31d23c866cb85d03cb68594
Mathis Jibert <mathis.jibert@gmail.com>
2023-02-28 17:17:56 +0100

    Controls Rect Transform

--- Mobile_G3/Assets/GridControlManager.cs
+++ Mobile_G3/Assets/GridControlManager.cs
@@ -1,15 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GridControlManager : MonoSingleton<GridControlManager>
 {
     public bool upKeyPressed,downKeyPressed,leftKeyPressed,rightKeyPressed;
+    public RectTransform[] buttons;
 
     public void OnKeyPressed(int key)
     {
-        Debug.Log("Oui");
         switch (key)
         {
             case 0:
@@ -24,6 +26,27 @@ public class GridControlManager : MonoSingleton<GridControlManager>
             case 3:
                 rightKeyPressed = true;
                 break;
+        }
+    }
+    
+    [UsedImplicitly]
+    public void OnTapOnScreen(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            TapOnScreen(Input.mousePosition);
+        }
+    }
+
+    void TapOnScreen(Vector2 position)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (buttons[i].rect.Contains(position-(Vector2)buttons[i].position))
+            {
+                OnKeyPressed(i);
+                break;
+            }
         }
     }
 
