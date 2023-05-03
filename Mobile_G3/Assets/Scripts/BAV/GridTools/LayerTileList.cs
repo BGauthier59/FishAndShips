9eec7f57e856fb9cc797320bb1cbd456eda87512
BaptisteV <86929180+MadBrosos@users.noreply.github.com>
2023-05-03 17:26:58 +0200

    Push to Push the Save

---
+++ Mobile_G3/Assets/Scripts/BAV/GridTools/LayerTileList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerTileList : MonoBehaviour
+{
+    public List<Tile> layerTileData = new List<Tile>();
+}
