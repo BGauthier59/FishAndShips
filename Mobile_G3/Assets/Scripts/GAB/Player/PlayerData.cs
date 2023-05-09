eac807359ae64cda862dadc9b868a86f64724307
BGauthier59 <g.baudinet@rubika-edu.com>
2023-05-09 14:30:58 +0200

    Import characters

---
+++ Mobile_G3/Assets/Scripts/GAB/Player/PlayerData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public Animation animation;
+    
+    public AnimationClip actionClip, jumpClip, idleClip;
+
+    public void PlayActionAnimation()
+    {
+        
+    }
+}
