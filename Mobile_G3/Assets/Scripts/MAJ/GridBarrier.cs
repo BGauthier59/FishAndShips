4a7ec6ad43ec5a56569f52debb9d84e1756b4000
BGauthier59 <g.baudinet@rubika-edu.com>
2023-05-21 23:57:53 +0200

    level 3 integrated

--- Mobile_G3/Assets/Scripts/MAJ/GridBarrier.cs
+++ Mobile_G3/Assets/Scripts/MAJ/GridBarrier.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class GridBarrier : MonoBehaviour
@@ -8,18 +9,17 @@ public class GridBarrier : MonoBehaviour
     public bool isClosed;
 
     public Transform barrier;
-    public Vector3 closedPos,openPos;
+    public Vector3 closedPos, openPos;
     
-
     private void Update()
     {
         if (isClosed)
         {
-            barrier.position = Vector3.Lerp(barrier.position,closedPos,5*Time.deltaTime);
+            barrier.position = Vector3.Lerp(barrier.position, closedPos, 5 * Time.deltaTime);
         }
         else
         {
-            barrier.position = Vector3.Lerp(barrier.position,openPos,5*Time.deltaTime);
+            barrier.position = Vector3.Lerp(barrier.position, openPos, 5 * Time.deltaTime);
         }
     }
 }
